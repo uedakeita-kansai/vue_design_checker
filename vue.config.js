e56cd816993ce82326559fc8d51e7106ac0dcec9
uedakeita-kansai <89079365+uedakeita-kansai@users.noreply.github.com>
2025-01-03 22:21:20 +0900

    [deploy]deploy準備

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  outputDir: 'docs',
+  publicPath: './',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
